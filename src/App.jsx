aa84788d65aebaf8c51ad114e04327b3ced96beb
TejashPithadiya <tejashpithadiya5951@gmail.com>
2024-06-14 16:23:44 +0530

    my f

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/demo1'>
       <Navbar/>
       <Routes>
       <Route path='/' element={<Home/>}/>
